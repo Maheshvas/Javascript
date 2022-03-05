5cab313ed196ed5b1d2d2f4ad8098f7d56d10b61
Maheshvas <maheshvasa567@gmail.com>
2022-03-06 00:00:56 +0530

    assignment4 submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,18 @@
 function CreateObject(arr) {
     // Write your code here
+    let obj={};
+    let value=[];
+    let key=[];
+    for(let i=0;i<arr.length;i++){
+        if(i%2==1){
+            value.push(arr[i]);
+        }else{
+            key.push(arr[i])
+        }
+
+    }key.forEach((key,i)=>obj[key]=value[i])
+    return obj
+    
 }
 
 module.exports = CreateObject;
